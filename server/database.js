d95a7db2639e4f500187453f7cb08f0ad721ea57
Evair F S Filho <evair.juninho@gmail.com>
2026-02-21 16:07:48 -0400

    main

--- server/database.js
+++ server/database.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',      // ou 127.0.0.1
-  user: 'root',
-  password: '',  // senha correta
-  database: 'lucina69_animalhelp'       // nome do seu banco
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
+
+module.exports = pool.promise();
 
 connection.connect(err => {
   if (err) {
