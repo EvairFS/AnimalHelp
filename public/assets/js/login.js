e5ce3aa3aca655a373269c0a7d86834bc598ee79
Evair F S Filho <evair.juninho@gmail.com>
2026-02-21 19:32:50 -0400

    Teste

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -29,30 +29,16 @@ function login() {
     return;
   }
 
-  fetch(`${API_URL}/auth/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, senha })
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (data.token) {
-        localStorage.setItem("token", data.token);
-        window.location.href = "/admin.html";
-      } else if (data.erro) {
-        erroEl.textContent = data.erro;
-
-        if (data.erro.toLowerCase().includes("senha")) {
-          senhaEl.classList.add("input-erro");
-          setTimeout(() => senhaEl.classList.remove("input-erro"), 500);
-        } else if (data.erro.toLowerCase().includes("usuário")) {
-          emailEl.classList.add("input-erro");
-          setTimeout(() => emailEl.classList.remove("input-erro"), 500);
-        }
-      } else {
-        erroEl.textContent = "Erro desconhecido";
-      }
-    });
+fetch(`${API_URL}/auth/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ email, senha })
+})
+.then(res => res.text())
+.then(text => {
+  console.log("RESPOSTA BRUTA:", text);
+})
+.catch(console.error);
 }
 
 /* 👇 ADICIONE ISSO */
